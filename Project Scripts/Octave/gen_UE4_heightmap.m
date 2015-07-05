0a388614a05cd12b428206f29ca962915cc9119e
hillites@hotmail.com <jumper13>
2015-07-05 15:34:45 +1000

    Height map edits

--- Project Scripts/Octave/gen_UE4_heightmap.m
+++ Project Scripts/Octave/gen_UE4_heightmap.m
@@ -1,10 +1,12 @@
 %gen_UE4_heightmap.m
-%Ocatave script -  Converts Elevation data to a suitable scaled form for 
-%integration in the Unreal 4 degin engine
+%Ocatave script -  Converts elevation data to a form suitable scaled for 
+%integration into the unreal engine platform
 %Modified: 05/07/2015
 %Req. Packages: image
 
-%Dimension of input elevation file
+pkg load image
+
+%Dimensions of elevation data
 inputdim = [1009 1009];
 %input file name - raw elevation data  (*.bin)
 filename = 'adelaide30km_0.bin';
@@ -16,7 +18,7 @@ fid = fopen(filename);
 heights = fread(fid, inputdim, 'short');
 fclose(fid);
 
-%Convert negative elevations to sea level
+%Convert negative elevations to sea level i.e., 0
 for x = 1 : size(heights, 1)
     for y = 1 : size(heights, 2)
        if heights(x,y) < 0
@@ -28,12 +30,14 @@ end
 heights = fliplr(heights);
 
 % rescale to ue4 height range
-scalez = 200; %Scale factor (100% equivalent to 256m in UE4)
+scalez = 200; %Scale factor (100% scale equivalent to [-256, 256] heights m in UE4)
+
+%scale heights for the unreal engine
 heightsue4 = heights * 32768/(256*scalez/100) + 32768; 
 
 %interploate elevation data to UE4 dimensions
 %interplation methods - linear, bilinear, cubic, bicubic.
-%Note: depending on the system certain high order interploation schemes may 
+%Note: depending on the system high order interploation schemes may 
 %not work due to memory limitations
 heightsue4 = imresize(heightsue4, [4033 4033], 'bilinear');
 
